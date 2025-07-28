a8c0269e2d9db765680a17613eeed76898994781
Syed Raza hussain <122215499+Raza964866@users.noreply.github.com>
2025-07-28 21:21:24 +0500

    config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_very_secret_key_that_no_one_will_ever_guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:Door%401234@localhost/shadi'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(hours=2)
     
@@ -17,13 +17,13 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'syedrazah76@gmail.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'dswo gubk ocba qduy'
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or 'syedrazah76@gmail.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or ''
     
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'mysql://root:Door%401234@localhost/shadi'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or '
 
 class ProductionConfig(Config):
     DEBUG = False
